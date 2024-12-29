ad56ac860b71153534ce51cd5fa598fed9eb6598
Mike Kistler <mikekistler@microsoft.com>
2024-12-29 10:21:52 -0600

    Repro for issue 59669.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using System.Runtime.Intrinsics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Add OpenApi services for two different versions
+builder.Services.AddOpenApi("v1");
+builder.Services.AddOpenApi("v2");
+
+// Add API versioning services
+builder.Services.AddApiVersioning();
 
 var app = builder.Build();
+
+var v1 = app.NewVersionedApi().HasApiVersion(1, 0);
+var v2 = app.NewVersionedApi().HasApiVersion(2, 0);
+
+v1.MapGet("hello", () => "Hello world").WithName("Hello");
+
+v2.MapGet("hello", () => "Hello world v2").WithName("Hello");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,28 +28,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # aspnet-versioning
-Explore support for versioning in ASP.NET Web APIs
+
+Explore support for versioning in ASP.NET Web APIs using the `Asp.Versioning` package.
+
+In the initial attempt to add versioning, we implemented api versioning using a query parameter
+named `api-version` with date-based version values '2022-01-01' and '2023-01-01'.
+Then we implemented one endpoint, /hello", that is defined for both API versions, and a
+second endpoint, "goodbye", that is defined for only API version 2023-01-01.
+
+So far:
+
+- The `/hello' endpoint works correctly for both API versions.
+- The `/goodbye` endpoint works correctly for API version 2023-01-01.
+- An OpenAPI document is generated for both versions.
+
+But there are some issues:
+
+- Calling either endpoint without the `api-version` query parameter results in a 400 error
+with no error message and no 'supported-api-versions' header.
+- Calling either endpoint with an `api-version` query parameter that is not one of the defined
+versions results in a 400 error with no error message and no 'supported-api-versions' header.
+- The OpenAPI document for 2022-01-01 includes the `/goodbye` endpoint, which is not supported
+for that version.
+- The `api-version` query parameter is not included in the OpenAPI document for any endpoint
+in either version.
